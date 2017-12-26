4db5e62aec7d2ab425d07ba7756dce80e583d5b0
zhangqian <415677303@qq.com>
2017-12-26 16:56:03 +0800

    order

---
+++ src/main/java/com/zack/controller/backend/OrderManagerController.java
@@ -0,0 +1,99 @@
+package com.zack.controller.backend;
+
+import com.github.pagehelper.PageInfo;
+import com.zack.common.Const;
+import com.zack.common.ResponseCode;
+import com.zack.common.ServerResponse;
+import com.zack.model.User;
+import com.zack.service.IOrderService;
+import com.zack.service.IUserService;
+import com.zack.vo.OrderVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+@RequestMapping("/manage/order/")
+@Controller
+public class OrderManagerController {
+
+    @Autowired
+    private IUserService iUserService;
+
+    @Autowired
+    private IOrderService iOrderService;
+
+    @RequestMapping(value = "list.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<PageInfo> orderList(HttpSession session, @RequestParam(value = "pageNum", defaultValue = "1") int pageNum,
+                                              @RequestParam(value = "pageSize", defaultValue = "10") int pageSize) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录,请登录管理员");
+
+        }
+        if (iUserService.checkAdminRole(user).isSuccess()) {
+            //填充我们增加产品的业务逻辑
+            return iOrderService.manageList(pageNum, pageSize);
+        } else {
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+    }
+
+    @RequestMapping(value = "detail.do")
+    @ResponseBody
+    public ServerResponse<OrderVo> orderDetails(HttpSession httpSession, Long orderNo) {
+
+        User user = (User) httpSession.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录,请登录管理员");
+
+        }
+        if (iUserService.checkAdminRole(user).isSuccess()) {
+            //填充我们增加产品的业务逻辑
+
+            return iOrderService.manageDetail(orderNo);
+        } else {
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+    }
+
+    @RequestMapping("search.do")
+    @ResponseBody
+    public ServerResponse<PageInfo> orderSearch(HttpSession session, Long orderNo, @RequestParam(value = "pageNum", defaultValue = "1") int pageNum,
+                                                @RequestParam(value = "pageSize", defaultValue = "10") int pageSize) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录,请登录管理员");
+
+        }
+        if (iUserService.checkAdminRole(user).isSuccess()) {
+            //填充我们增加产品的业务逻辑
+            return iOrderService.manageSearch(orderNo, pageNum, pageSize);
+        } else {
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+    }
+
+    @RequestMapping("send_goods.do")
+    @ResponseBody
+    public ServerResponse<String> orderSendGoods(HttpSession session, Long orderNo) {
+
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录,请登录管理员");
+
+        }
+        if (iUserService.checkAdminRole(user).isSuccess()) {
+            //填充我们增加产品的业务逻辑
+            return iOrderService.manageSendGoods(orderNo);
+        } else {
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+    }
+
+}
